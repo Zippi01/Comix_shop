c4d2a213629f517d5ab45b9c2cf552115c0be8de
Zippi01 <katyakovtun080619@gmail.com>
2019-12-17 19:05:55 +0200

    First commit

---
+++ app/controllers/product_controller.rb
@@ -0,0 +1,15 @@
+class ProductController < ApplicationController
+  before_action :set_product, only: [:show]
+
+  def index
+    @products = Product.all.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+  end
+
+  def show
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(title: "Book", description:"Book Collections")
+Category.create(title: "Magazine", description:"Magazine Collections")
+Category.create(title: "Manga", description:"Manga Collections")
+
+10.times do |i|
+  Product.create(title: "Book #{i}", description: "Book", price: 1+i, category_id: 1)
+end
+
+10.times do |i|
+  Product.create(title: "Magazine #{i}", description: "Magazine", price: 2+i, category_id: 2)
+end
+
+10.times do |i|
+  Product.create(title: "Manga #{i}", description: "Manga", price: 3+i, category_id: 3)
+end
